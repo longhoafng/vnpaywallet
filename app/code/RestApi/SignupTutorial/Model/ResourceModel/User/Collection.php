ced210507958e65d82e3b5a4a65e37e424cac1d8
longhoafng <69591191+longhoafng@users.noreply.github.com>
2023-09-30 15:02:08 +0700

    Delete app/code/RestApi/SignupTutorial directory

--- app/code/RestApi/SignupTutorial/Model/ResourceModel/User/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace RestApi\SignupTutorial\Model\ResourceModel\User;
-
-class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection{
-	public function _construct(){
-		$this->_init("RestApi\SignupTutorial\Model\User","RestApi\SignupTutorial\Model\ResourceModel\User");
-	}
-}
